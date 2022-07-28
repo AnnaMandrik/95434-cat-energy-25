bba06fcabcb0b2af286ca29c01a029ce66bbedda
Anna Mandrik <AnnaMandrik1984@yandex.ru>
2022-07-29 00:00:37 +0300

    Слайдер

--- source/js/app.js
+++ source/js/app.js
@@ -1,8 +1,57 @@
 import {getMenuOpenClose} from './menu';
-//import {getSliderMove} from './slider';
+import {getInstanceBefore} from './slider';
+
+
+// const mainNav = document.querySelector('.main-nav');
+// const mainNavToggle = document.querySelector('.main-header__toggle');
+// const mainConteiner = document.querySelector('.main-container');
+
+// mainNav.classList.remove('main-nav--nojs');
+// mainNavToggle.classList.remove('main-header__toggle--nojs');
+// mainConteiner.classList.remove('main-container--nojs');
+
+// const getMenuOpenClose = () => {
+//   mainNavToggle.addEventListener('click', handleChangeMenuButton);
+// }
+
+// function handleChangeMenuButton() {
+//   if (mainNav.classList.contains('main-nav--closed') && mainNavToggle.classList.contains('main-header__toggle--closed')) {
+//     mainNav.classList.remove('main-nav--closed');
+//     mainNavToggle.classList.remove('main-header__toggle--closed');
+//     mainNav.classList.add('main-nav--opened');
+//     mainNavToggle.classList.add('main-header__toggle--opened');
+//   } else {
+//     mainNav.classList.add('main-nav--closed');
+//     mainNavToggle.classList.add('main-header__toggle--closed');
+//     mainNav.classList.remove('main-nav--opened');
+//     mainNavToggle.classList.remove('main-header__toggle--opened');
+//   }
+// }
+
+
+// const sliderButton = document.querySelector('.slider__button');
+// const instanceLabel = document.querySelector('.instance__label');
+// const instanceImage = document.querySelector('.instance__image');
+// const imageCat = document.querySelector('.image-cat');
+
+// const getInstanceBeforeAfter = () => {
+//   instanceLabel.addEventListener('click', handleChangeInstanceControl);
+// }
+
+// function handleChangeInstanceControl() {
+//   if (instanceLabel.classList.contains('instance__label--after')) {
+//     instanceImage.style.display = 'none';
+//     imageCat.style.display = 'inline';
+//     sliderButton.style.right = '24px';
+
+//   } else {
+//     instanceImage.style.display = 'inline';
+//     imageCat.style.display = 'none';
+//     sliderButton.style.right = '0';
+//   }
+// }
+
+
 
 getMenuOpenClose();
-//getSliderMove();
-
-const slider = document.querySelector('.slider');
-const sliderButton = slider.querySelector('.slider__button');
+getInstanceBefore();
--- source/js/slider.js
+++ source/js/slider.js
@@ -1,39 +1,75 @@
-const slider = document.querySelector('.slider');
-const sliderButton = slider.querySelector('.slider__button');
+//const slider = document.querySelector('.slider');
+const sliderButton = document.querySelector('.slider__button');
+const instanceLabelBefore = document.querySelector('.instance__label--after');
+const instanceImage = document.querySelector('.instance__image');
+const imageCat = document.querySelector('.image-cat');
 
-const getSliderMove = () => {
-  console.log('hello world');
-  sliderButton.onmousedown = function(event) {
-    event.preventDefault();
+// const getInstanceBeforeAfter = () => {
+//   instanceLabel.addEventListener('click', handleChangeInstanceControl);
+// }
 
-    const shiftX = event.clientX - sliderButton.getBoundingClientRect().left;
+// function handleChangeInstanceControl() {
+//   if (instanceLabel.classList.contains('instance__label--after')) {
+//     instanceImage.style.display = 'none';
+//     imageCat.style.display = 'inline';
+//     sliderButton.style.right = '24px';
 
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
+//   } else {
+//     instanceImage.style.display = 'inline';
+//     imageCat.style.display = 'none';
+//     sliderButton.style.right = '0';
+//   }
+// }
+const getInstanceBefore = () => {
+  instanceLabelBefore.addEventListener('click', function() {
+    if (instanceLabel.classList.contains('instance__label--after')) {
+      instanceImage.style.display = 'none';
+      imageCat.style.display = 'inline';
+      sliderButton.style.right = '24px';
 
-    function onMouseMove(event) {
-      const newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
-
-      if (newLeft < 0) {
-        newLeft = 0;
-      }
-      const rightEdge = slider.offsetWidth - sliderButton.offsetWidth;
-      if (newLeft > rightEdge) {
-        newLeft = rightEdge;
-      }
-
-      sliderButton.style.left = newLeft + 'px';
+    } else {
+      instanceImage.style.display = 'inline';
+      imageCat.style.display = 'none';
+      sliderButton.style.right = '0';
     }
+});
 
-    function onMouseUp() {
-      document.removeEventListener('mouseup', onMouseUp);
-      document.removeEventListener('mousemove', onMouseMove);
-    }
-  }
-
-  sliderButton.ondragstart = function() {
-    return false;
-  }
 }
 
-export {getSliderMove};
+
+
+// const getSliderMove = () => {
+//   console.log('hello world');
+//   sliderButton.onmousedown = function(event) {
+//     event.preventDefault();
+
+//     const shiftX = event.clientX - sliderButton.getBoundingClientRect().left;
+
+//     document.addEventListener('mousemove', onMouseMove);
+//     document.addEventListener('mouseup', onMouseUp);
+
+//     function onMouseMove(event) {
+//       const newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
+
+//       if (newLeft < 0) {
+//         newLeft = 0;
+//       }
+//       const rightEdge = slider.offsetWidth - sliderButton.offsetWidth;
+//       if (newLeft > rightEdge) {
+//         newLeft = rightEdge;
+//       }
+
+//       sliderButton.style.left = newLeft + 'px';
+//     }
+
+//     function onMouseUp() {
+//       document.removeEventListener('mouseup', onMouseUp);
+//       document.removeEventListener('mousemove', onMouseMove);
+//     }
+//   }
+
+//   sliderButton.ondragstart = function() {
+//     return false;
+//   }
+// }
+export default {getInstanceBefore}
